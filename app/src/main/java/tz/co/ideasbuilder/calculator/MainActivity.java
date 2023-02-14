57d50b4712172d53849b7fad48fdc15e2128d85c
chax255 <ronald.chaula@live.com>
2023-02-14 21:16:47 +0300

    added the whipped cream and chocolate checkboxes, also created some functionalities for each button and new order summary

--- app/src/main/java/tz/co/ideasbuilder/calculator/MainActivity.java
+++ app/src/main/java/tz/co/ideasbuilder/calculator/MainActivity.java
@@ -1,10 +1,14 @@
 package tz.co.ideasbuilder.calculator;
 
 import android.annotation.SuppressLint;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +16,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
     }
 //this method is called when the button is clicked on the front end of the website
     int currentOrderSize = 0;
     int quantity;
+    String message;
+    long unitPrice = 5;
 
     public void increaseOrder(View view){
         showIncrease();
@@ -25,24 +35,33 @@ public class MainActivity extends AppCompatActivity {
         showDecrease();
     }
     public void submitOrder(View view){
-        displayPrice(quantity);
+        message = setTopping();
+        displayPrice(quantity, message);
+
+        Log.i("MainActivity",message);
+
     }
 
     //the price method is declared here
     @SuppressLint("SetTextI18n")
-    public void displayPrice(int unit){
-        int unitPrice = 5;
+    private void displayPrice(int unit, String additions){
+
         //the line below captures a textview that is to be affected by using findViewById
-        TextView price = (TextView) findViewById(R.id.price);
+        TextView price = findViewById(R.id.price);
         //the below line is used to write to the text view through the object
-        price.setText("$" + unitPrice*unit);
+        if (unit>0) {
+            price.setText("You have ordered: " + unit + " cups \n" + "Total Price is: " + NumberFormat.getCurrencyInstance().format(unitPrice * unit) + "\n" + additions + "\n"+ "Thank you!");
+        }
+        else{
+            price.setText("Please make your order");
+        }
     }
 
-    public int increase(){
+    private int increase(){
         return currentOrderSize+=1;
 
     }
-    public int decrease(){
+    private int decrease(){
         if(quantity<=0){
 
             return  0;
@@ -52,18 +71,52 @@ public class MainActivity extends AppCompatActivity {
         }
     }
     @SuppressLint("SetTextI18n")
-    public void showIncrease(){
+    private void showIncrease(){
         quantity = increase();
-        TextView content = (TextView) findViewById(R.id.quantity);
+        TextView content = findViewById(R.id.quantity);
         content.setText(" " +  quantity);
 
     }
     @SuppressLint("SetTextI18n")
-    public void showDecrease(){
+    private void showDecrease(){
         quantity = decrease();
-        TextView content = (TextView) findViewById(R.id.quantity);
+        TextView content = findViewById(R.id.quantity);
         content.setText(" " +  quantity);
 
     }
+public String setTopping(){
+        if (hasWhippedCream() && hasChocolate()){
+            unitPrice = 10;
+            return "Topping chosen is: Whipped Cream & Chocolate";
+        }
+
+
+    else if(hasChocolate()){
+            unitPrice =8;
+            return "Topping is: Chocolate";
+        }
+
+        else if (hasWhippedCream()){
+            unitPrice = 7;
+            return "Topping chosen is: whipped cream";
+        }
+    else{
+            unitPrice = 5;
+            return "No toppings selected";
+        }
+
+}
+
+    private boolean hasChocolate() {
+        CheckBox choco = (CheckBox) findViewById(R.id.choco);
+        return choco.isChecked();
+    }
+
+    private boolean hasWhippedCream() {
+        CheckBox whippedCream = (CheckBox) findViewById(R.id.whippedCream);
+        return whippedCream.isChecked();
+    }
+
+
 }
 
